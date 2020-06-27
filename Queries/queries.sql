9aab85643705ba167d178ca0050a3e9b28597bfa
tienl <tien.lee@gmail.com>
2020-06-27 00:38:30 -0700

    deliverable 1

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -72,11 +72,201 @@ SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
 
--- Retirement eligibility
-SELECT first_name, last_name
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
 
-select * FROM retirement_info
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+
+SELECT ri.emp_no,
+	ri.first_name,
+ri.last_name,
+	de.to_date 
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+	
+	
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+select * from current_emp;
+
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO newopenings 
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no 
+
+select * from newopenings
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+
+SELECT emp_no,
+	first_name,
+	last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+drop table emp_info
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	      AND (de.to_date = '9999-01-01');
+		  
+		  
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+		
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name	
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+SELECT re.emp_no, 
+	re.first_name,
+	re.last_name,
+	d.dept_name
+--INTO drill
+FROM retirement_info AS re
+INNER JOIN dept_emp AS de
+ON (re.emp_no = de.emp_no)
+--sales = d007 development = d005
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_no IN ('d007','d005')
+
+
+
+
+select * from departments
+	
+	select *from dept_emp
+drop table deliv1
+
+--deliverable 1 number of retiring employee by title 
+-- retiring = 1952-1955 
+-- group by title 
+-- inner join 
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	ti.title, 
+	ti.to_date,
+	s.salary	
+INTO deliv1
+FROM employees as e
+INNER JOIN titles as ti 
+ON (e.emp_no = ti.emp_no)
+INNER JOIN salaries as s 
+ON (ti.emp_no = s.emp_no)
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- finding duplicates from deliv1 
+select emp_no, first_name, count(*) 
+from deliv1 
+group by emp_no, first_name
+having count (*) > 1
+
+select * from titles where emp_no = 10011
+-- Partition the data to show only most recent title per employee
+SELECT emp_no,
+ first_name,
+ last_name,
+ title,
+ to_date,
+ salary
+--INTO deliv1_2
+FROM
+ (SELECT emp_no,
+ first_name,
+ last_name,
+ title,
+ to_date,
+ salary,
+  ROW_NUMBER() OVER
+ (PARTITION BY (emp_no)
+ ORDER BY to_date DESC) rn
+ FROM deliv1
+ ) tmp WHERE rn = 1
+ORDER BY emp_no;
+
